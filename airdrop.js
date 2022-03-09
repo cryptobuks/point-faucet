ad7e0ebb32408ceabea32d7afcb625c2870d6618
den <d.n.voropaev@gmail.com>
2022-03-09 11:17:50 +0300

    implemented transaction queue

--- airdrop.js
+++ airdrop.js
@@ -1,5 +1,44 @@
 const web3 = require("web3");
 const utils = require("./utils");
+const config = require('./config.json');
+
+class TransactionError extends Error {};
+class RequestError extends Error {};
+
+const getTxQueue = () => {
+    const queue = [];
+    const start = async () => {
+        while (true) {
+            const nextTask = queue.shift();
+            if (nextTask) {
+                await nextTask();
+            } else {
+                await utils.sleep(config.queue.cooldown);
+            }
+        }
+    };
+
+    const push = task => {
+        console.log('Pushing new task', {task, queue: queue.length})
+        if (queue.length > config.queue.maxCapacity) {
+            throw new TransactionError('Too many requests, please try again later');
+        }
+        return new Promise((resolve, reject) => {
+            queue.push(async () => {
+                try {
+                    resolve(await task());
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
+    };
+
+    return {start, push};
+}
+
+const txQueue = getTxQueue();
+txQueue.start();
 
 class Airdrop {
     constructor(config, secrets, provider) {
@@ -9,31 +48,42 @@ class Airdrop {
     }
 
     index = async(req, res) => {
-        const airdropAddress = this.secrets.address;
-        const airdropPrivateKey = this.secrets.privKey;
-        const toAddress = req.query.address;
-        if (! utils.ethAddressRegex.test(toAddress)) throw new Error('Invalid address: '+toAddress);
+        try {
+            const toAddress = req.query.address;
 
-        const amount = this.config.airdrop.amount;
-        if (!amount || amount <= 0) throw new Error('Invalid amount in config: '+amount);
+            if (! utils.ethAddressRegex.test(toAddress)) {
+                throw new RequestError('Invalid address: '+toAddress);
+            }
 
-        if (toAddress === airdropAddress) throw new Error('Cannot send to itself');
+            if (toAddress === this.secrets.address) {
+                throw new RequestError('Cannot send to itself');
+            }
 
-        const createTransaction = await this.provider.eth.accounts.signTransaction(
-            {
-                from: airdropAddress,
-                to: toAddress,
-                value: web3.utils.toWei(amount.toString(), 'ether'),
-                gas: '21000',
-            },
-            airdropPrivateKey
-        );
+            const amount = this.config.airdrop.amount;
+            const createReceipt = await txQueue.push(async () => {
+                const options = {
+                    from: this.secrets.address,
+                    to: toAddress,
+                    value: web3.utils.toWei(amount.toString(), 'ether'),
+                    gas: '21000',
+                };
 
-        const createReceipt = await this.provider.eth.sendSignedTransaction(
-            createTransaction.rawTransaction
-        );
+                console.log('Creating a new tx:', options);
 
-        res.send({ "amount": amount, "txid": createReceipt.transactionHash });
+                const tx = await this.provider.eth.accounts.signTransaction(options, this.secrets.privKey);
+
+                console.log('Sending a new tx:', tx);
+
+                return this.provider.eth.sendSignedTransaction(
+                    tx.rawTransaction
+                )
+            });
+
+            res.send({status: 'ok', amount, txid: createReceipt.transactionHash});
+        } catch (e) {
+            console.log('Airdrop error:', e);
+            res.send({status: 'error', code: e.constructor.name, message: e.message});
+        }
     }
 }
 
--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
 module.exports = {
-    ethAddressRegex: new RegExp(/^0x[a-fA-F0-9]{40}$/)
+    ethAddressRegex: new RegExp(/^0x[a-fA-F0-9]{40}$/),
+    sleep: (time) => new Promise((resolve) => setTimeout(resolve, time))
 }
